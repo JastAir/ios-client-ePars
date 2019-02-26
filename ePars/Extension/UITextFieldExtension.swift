9b2cb7cb4b726483e2faed00f887dd810a5ae779
Sergey Neikovich <s_neiko@outlook.com>
2019-02-27 00:25:16 +0300

    Complete UITextField customise

--- ePars/Extension/UITextFieldExtension.swift
+++ ePars/Extension/UITextFieldExtension.swift
@@ -11,11 +11,28 @@ import UIKit
 extension UITextField {
 
 	func setHint(text: String){
-		self.layer.cornerRadius = 5.0
-		self.layer.borderColor =
-			UIColor(red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+		self.layer.borderColor = UIColor(red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0).cgColor
 		self.layer.borderWidth = 1.0
+		self.layer.cornerRadius = 5.0
+		self.layer.backgroundColor = UIColor.white.cgColor
 		
 		
+		let hintUILabel = UILabel(frame:
+			CGRect(x: self.frame.width, y: 0, width: 100, height: 100)
+		)
+		hintUILabel.text = text
+		hintUILabel.sizeToFit()
+		
+		hintUILabel.center = CGPoint(
+			x: (self.frame.width - (hintUILabel.frame.width / 2)) - 8,
+			y: hintUILabel.frame.height / 2 + 6
+		)
+		
+		hintUILabel.textAlignment = .right
+		hintUILabel.font = UIFont.systemFont(ofSize: 12)
+		hintUILabel.textColor = UIColor.lightGray
+		hintUILabel.padding = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+		
+		self.addSubview(hintUILabel)
 	}
 }
